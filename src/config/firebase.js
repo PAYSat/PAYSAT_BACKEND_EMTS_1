4d3c2e521299f641ab8b72cf4edcfcd8ebd9207d
Edgar <marce.tapias007@gmail.com>
2025-10-30 09:06:30 -0500

    002_backend_base_listo

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -8,4 +8,25 @@ if (!admin.apps.length) {
   });
 }
 
-export const db = admin.firestore();
+// export const db = admin.firestore();
+export { admin };
+
+
+
+
+
+
+
+// import admin from 'firebase-admin';
+
+// const projectId = process.env.FIREBASE_PROJECT_ID;
+// const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+// const privateKey = (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n');
+
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert({ projectId, clientEmail, privateKey })
+//   });
+// }
+
+// export { admin };
